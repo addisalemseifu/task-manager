46a0ac5dec5e56663aa0e7573bb1124026cd8542
Addis <addisalemseifu@gmail.com>
2026-03-18 20:36:02 -0400

    configured-for-render

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,53 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import taskRoutes from "./routes/taskRoutes.js"
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+
+// dotenv.config();
+
+// connectDB()
+
+// const app = express();
+
+// // middleware
+// app.use(express.json());
+// app.use(cors());
+
+// // routes
+// app.use('/api/auth',authRoutes)
+// app.use('/api/tasks',taskRoutes)
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js"
+import taskRoutes from "./routes/taskRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
-connectDB()
+// connect DB
+connectDB();
 
 const app = express();
 
@@ -16,13 +56,23 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use('/api/auth',authRoutes)
-app.use('/api/tasks',taskRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+// dirname fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// ⭐ FIXED wildcard route
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
 });
 
+
+// port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
