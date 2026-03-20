b4a7dc2e0e13a6c56289fb4b04866beb628aa3d2
Addis <addisalemseifu@gmail.com>
2026-03-19 21:35:01 -0400

    Fix-connection-string

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "/api/auth/login",
         user
       );
 
